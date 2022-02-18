9d000030c47f3a3b2d0a2b5d0a9e26cfeea96e22
Linnea Hensfelt Andersson <linneahensfelta@gmail.com>
2022-02-18 18:27:50 +0100

    map på cards

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Card } from "../components/Card";
 
 const Home = () => {
+  const { cards } = useSelector((state) => state.wallet);
+
   return (
     <div>
       <h1>E-Wallet</h1>
       <p>Active card</p>
-      <Link to="/addcard">
+      {cards.map((card, i) => {
+        return <Card {...card} key={i} />;
+      })}
+      <Link
+        to="/addcard"
+        onClick={(e) => {
+          if (cards.length >= 4) {
+            document.querySelector("#error").innerHTML = "You have to many cards!"
+            e.preventDefault();
+          }
+        }}
+      >
+      <p id="error"></p>
         <button>Add a new card</button>
       </Link>
     </div>
